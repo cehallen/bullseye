3464e771f43b2bf0b2ec61d25c7cff420d2fd473
Chris Allen <cehallen@gmail.com>
2016-02-10 12:17:55 -0500

    to p 60

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50
+    var currentValue: Int = 0
     @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
